3d694c4aa1657dd508d201e44e9cad0632be17a9
Azamat <azamatasadillayev@gmail.com>
2023-06-10 17:33:24 +0500

    lola

---
+++ Untitled1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main(){
+	
+   int age;
+   cout << " Yoshingizni kiriting: ";
+   cin>>age;
+
+    if(age<18){
+    cout << " Saylovda qatnashaolmaysiz";
+    }else if (age>18){
+	cout << " Saylovda qatnashaolsiz";
+	}
+		
+	
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
+
